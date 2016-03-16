60207e6af50dc934f3a6bb28cc2eb3263e1af0a2
Gaspard Bucher <gaspard@lucidogen.io>
2016-03-16 11:57:47 +0100

    Started to move from harp app to webpack and Vue.js

---
+++ build/dev-client.js
@@ -0,0 +1,12 @@
+require ( 'eventsource-polyfill' )
+
+const hotClient =
+require ( 'webpack-hot-middleware/client?noInfo=true&reload=true' )
+
+hotClient.subscribe
+( ( event ) => {
+    if ( event.action === 'reload' ) {
+      window.location.reload ()
+    }
+  }
+)
---
+++ src/main.js
@@ -0,0 +1,9 @@
+import App from './App'
+import Vue from 'vue'
+
+/* eslint-disable no-new */
+new Vue
+( { el: 'body'
+  , components: { App }
+  }
+)
