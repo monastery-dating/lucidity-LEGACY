a23c1270b2ab576c897b679d2584192a7f87acec
Gaspard Bucher <gaspard@lucidogen.io>
2016-05-07 12:33:21 +0200

    Initial config.js

---
+++ desktop.js
@@ -0,0 +1,45 @@
+console.log ( 'LOADING DESKTOP' )
+/*
+import App from './desktop/App/index'
+import Controller from 'cerebral'
+import Devtools from 'cerebral-module-devtools'
+import Http from 'cerebral-module-http'
+import Model from 'cerebral-model-baobab'
+// import Library from './modules/Library'
+// import Project from './modules/Project'
+// import Router from 'cerebral-module-router'
+// import Scene from './modules/Scene'
+// import User from './modules/User'
+// We need Component because of jsx usage
+import { Component, render } from 'cerebral-view-snabbdom'
+
+const controller = Controller ( Model ( {} ) )
+
+// Our modules
+controller.addModules
+( {
+  }
+)
+// Default modules
+controller.addModules
+( { http: Http ()
+  , devtools: Devtools ()
+  */
+  /*
+  , router: Router
+    ( { '/': 'example.redirectRoot' // these are signals
+      }
+    , { onlyHash: true }
+    )
+    */
+    /*
+  }
+)
+
+render
+( () => <App/>
+, document.querySelector ( '#root' )
+, controller
+)
+*/
+const foo = <div>Hello World</div>
